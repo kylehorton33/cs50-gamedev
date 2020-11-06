0d00e7667785c067191912657c1461dcdf10b76c
Kyle <kylehorton33@gmail.com>
2020-11-06 11:29:41 -0500

    add more fruits to confirm functionality

--- learn-love/ch7/main.lua
+++ learn-love/ch7/main.lua
@@ -2,6 +2,9 @@ function love.load()
   --Each value is separated by a comma, just like with parameters and arguments
   fruits = {"apple", "banana"}
   table.insert(fruits, "pear")
+  table.insert(fruits, "pineapple")
+  table.insert(fruits, "tomato")
+  table.insert(fruits, "raspberry")
 end
 
 function love.draw()
