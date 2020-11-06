2b90f2bf20e8ec0deb9cd98875871245d7bc3e27
Kyle <kylehorton33@gmail.com>
2020-11-06 16:27:46 -0500

    add circle class based on shape

--- learn-love/ch11/main.lua
+++ learn-love/ch11/main.lua
@@ -1,11 +1,21 @@
 --! file: main.lua
 
+--! file: main.lua
+
 function love.load()
   Object = require "classic"
+  --Don't forget to load the file
   require "shape"
+
   require "rectangle"
+
+  --Don't forget to load the file
+  require "circle"
+
   r1 = Rectangle(100, 100, 200, 50)
-  r2 = Rectangle(350, 80, 25, 140)
+
+  --We make r2 a Circle instead of a Rectangle
+  r2 = Circle(350, 80, 40)
 end
 
 function love.update(dt)
