7e5a223e693149331484596552c0f8c642d75fd5
Kyle <kylehorton33@gmail.com>
2020-11-06 11:21:43 -0500

    move box in 2d with arrow keys

--- learn-love/ch6/main.lua
+++ learn-love/ch6/main.lua
@@ -1,5 +1,6 @@
 function love.load()
   x = 100 
+  y = 50
   move = true
 
 end
@@ -7,9 +8,15 @@ end
 function love.update(dt)
   if love.keyboard.isDown("right") then
     x = x + 100 * dt
+  elseif love.keyboard.isDown("left") then
+    x = x - 100 * dt
+  elseif love.keyboard.isDown("up") then
+    y = y - 100 * dt
+  elseif love.keyboard.isDown("down") then
+    y = y + 100 * dt
   end
 end
 
 function love.draw()
-  love.graphics.rectangle("line", x, 50, 200, 150)
+  love.graphics.rectangle("line", x, y, 200, 150)
 end
