aa27268c49e52efcc7110e9b78864eb5a2adec77
Kyle <kylehorton33@gmail.com>
2020-11-06 11:19:07 -0500

    move box with right arrow key

--- learn-love/ch6/main.lua
+++ learn-love/ch6/main.lua
@@ -1,16 +1,15 @@
 function love.load()
   x = 100 
-  deltaT = 0
+  move = true
+
 end
 
 function love.update(dt)
-  deltaT = dt -- approx 0.01 on this comp
-  if x < 600 then
-    x = x + 100*dt
+  if love.keyboard.isDown("right") then
+    x = x + 100 * dt
   end
 end
 
 function love.draw()
   love.graphics.rectangle("line", x, 50, 200, 150)
-  love.graphics.print(deltaT,100,300)
 end
