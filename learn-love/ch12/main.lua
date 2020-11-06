2edebfb34702fb5b641723b22ea5620a812e0b45
Kyle <kylehorton33@gmail.com>
2020-11-06 16:34:09 -0500

    playing wiht .draw() arguements

--- learn-love/ch12/main.lua
+++ learn-love/ch12/main.lua
@@ -1,7 +1,10 @@
 function love.load()
   myImage = love.graphics.newImage("sheep.png")
+  width = myImage:getWidth()
+  height = myImage:getHeight()
 end
 
 function love.draw()
-  love.graphics.draw(myImage, 100, 100)
+  -- change .draw() arguements
+  love.graphics.draw(myImage, 100, 100, 0, 2, 2, width/2, height/2)
 end
