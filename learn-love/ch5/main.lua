03f54fb266e54b3bd11704672b2c93e89a993b97
Kyle <kylehorton33@gmail.com>
2020-11-06 11:07:11 -0500

    update with dt to keep consistent across computers

--- learn-love/ch5/main.lua
+++ learn-love/ch5/main.lua
@@ -1,11 +1,14 @@
 function love.load()
   x = 100 
+  deltaT = 0
 end
 
-function love.update()
-  x = x + 5
+function love.update(dt)
+  deltaT = dt -- approx 0.01 on this comp
+  x = x + 100*dt
 end
 
 function love.draw()
   love.graphics.rectangle("line", x, 50, 200, 150)
+  love.graphics.print(deltaT,100,300)
 end
