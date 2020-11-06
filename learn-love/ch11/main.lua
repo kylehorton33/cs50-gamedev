4397c351c586be5ad6b62ebdbf6d99ab930da7e3
Kyle <kylehorton33@gmail.com>
2020-11-06 16:26:45 -0500

    abstract x,yspeed to shape class

--- learn-love/ch11/main.lua
+++ learn-love/ch11/main.lua
@@ -2,6 +2,7 @@
 
 function love.load()
   Object = require "classic"
+  require "shape"
   require "rectangle"
   r1 = Rectangle(100, 100, 200, 50)
   r2 = Rectangle(350, 80, 25, 140)
--- learn-love/ch11/rectangle.lua
+++ learn-love/ch11/rectangle.lua
@@ -1,22 +1,12 @@
 --! file: rectangle.lua
---Lua turns this into: Object.extend(Object)
-Rectangle = Object:extend()
+Rectangle = Shape:extend()
 
---Lua turns this into: Rectangle.new(self)
 function Rectangle:new(x, y, width, height)
-    self.x = x
-    self.y = y
+    Rectangle.super.new(self, x, y)
     self.width = width
     self.height = height
-    self.speed = 100
 end
 
---Lua turns this into: Rectangle.update(self, dt)
-function Rectangle:update(dt)
-    self.x = self.x + self.speed * dt
-end
-
---Lua turns this into: Rectangle.draw(self)
 function Rectangle:draw()
     love.graphics.rectangle("line", self.x, self.y, self.width, self.height)
 end
