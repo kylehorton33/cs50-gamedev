bac823766c27eb2a4622cd9333843c96e53f2f12
Kyle <kylehorton33@gmail.com>
2020-11-06 16:34:54 -0500

    playing with color

--- learn-love/ch12/main.lua
+++ learn-love/ch12/main.lua
@@ -1,10 +1,14 @@
 function love.load()
   myImage = love.graphics.newImage("sheep.png")
-  width = myImage:getWidth()
-  height = myImage:getHeight()
+  love.graphics.setBackgroundColor(1, 1, 1)
 end
 
 function love.draw()
-  -- change .draw() arguements
-  love.graphics.draw(myImage, 100, 100, 0, 2, 2, width/2, height/2)
+  love.graphics.setColor(255/255, 200/255, 40/255, 127/255)
+  love.graphics.setColor(1, 0.78, 0.15, 0.5)
+  -- Or ...
+  love.graphics.draw(myImage, 100, 100)
+  -- Not passing an argument for alpha automatically sets it to 1 again.
+  love.graphics.setColor(1, 1, 1)
+  love.graphics.draw(myImage, 200, 100)
 end
