4adc7d32df425f75a6a6a98c4fa0cb06223ddc4d
Kyle <kylehorton33@gmail.com>
2020-11-06 11:36:10 -0500

    create new rectangles when spacebar is pressed

--- learn-love/ch8/main.lua
+++ learn-love/ch8/main.lua
@@ -1,19 +1,35 @@
 function love.load()
+  -- Remember: camelCasing!
+  listOfRectangles = {}
+end
+
+function createRect()
   rect = {}
   rect.x = 100
   rect.y = 100
   rect.width = 70
   rect.height = 90
-
-  --Add a speed property
   rect.speed = 100
+
+  -- Put the new rectangle in the list
+  table.insert(listOfRectangles, rect)
+end
+
+function love.keypressed(key)
+  -- Remember, 2 equal signs (==) for comparing!
+  if key == "space" then
+      createRect()
+  end
 end
 
 function love.update(dt)
-  -- Increase the value of x. Don't forget to use delta time.
-  rect.x = rect.x + rect.speed * dt
+  for i,v in ipairs(listOfRectangles) do
+    v.x = v.x + v.speed * dt
+  end
 end
 
 function love.draw()
-  love.graphics.rectangle("line", rect.x, rect.y, rect.width, rect.height)
+  for i,v in ipairs(listOfRectangles) do
+    love.graphics.rectangle("line", v.x, v.y, v.width, v.height)
+  end
 end
