e970874b2b0d347aeb7e3e1abe4f7a8272daf2d9
Kyle <kylehorton33@gmail.com>
2020-11-06 17:08:09 -0500

    collision detection with bullets

--- learn-love/ch14/main.lua
+++ learn-love/ch14/main.lua
@@ -21,6 +21,13 @@ function love.update(dt)
 
   for i,v in ipairs(listOfBullets) do
       v:update(dt)
+      v:checkCollision(enemy)
+
+      --If the bullet has the property dead and it's true then..
+      if v.dead then
+          --Remove it from the list
+          table.remove(listOfBullets, i)
+      end
   end
 end
 
