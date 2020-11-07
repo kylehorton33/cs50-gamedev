51edfb1c0ef89fb71c159bb38a403a61aef8820e
Kyle <kylehorton33@gmail.com>
2020-11-06 23:40:10 -0500

    create coins and move player

--- learn-love/ch21/main.lua
+++ learn-love/ch21/main.lua
@@ -1,23 +1,33 @@
 function love.load()
-  -- Create a player object with an x, y and size
   player = {
       x = 100,
       y = 100,
       size = 25,
       image = love.graphics.newImage("face.png")
   }
+
+  coins = {}
+
+  for i=1,25 do
+      table.insert(coins,
+          {
+              -- Give it a random position with math.random
+              x = math.random(50, 650),
+              y = math.random(50, 450),
+              size = 10,
+              image = love.graphics.newImage("dollar.png")
+          }
+      )
+  end
 end
 
 function love.update(dt)
-  -- Make it moveable with keyboard 
   if love.keyboard.isDown("left") then
       player.x = player.x - 200 * dt
   elseif love.keyboard.isDown("right") then
       player.x = player.x + 200 * dt
   end
 
-  -- Note how I start a new if-statement instead of contuing the elseif
-  -- I do this because else you wouldn't be able to move diagonally.
   if love.keyboard.isDown("up") then
       player.y = player.y - 200 * dt
   elseif love.keyboard.isDown("down") then
@@ -27,7 +37,12 @@ end
 
 function love.draw()
   love.graphics.circle("line", player.x, player.y, player.size)
-  -- Set the origin of the face to the center of the image
   love.graphics.draw(player.image, player.x, player.y,
       0, 1, 1, player.image:getWidth()/2, player.image:getHeight()/2)
+
+  for i,v in ipairs(coins) do
+      love.graphics.circle("line", v.x, v.y, v.size)
+      love.graphics.draw(v.image, v.x, v.y,
+          0, 1, 1, v.image:getWidth()/2, v.image:getHeight()/2)
+  end
 end
