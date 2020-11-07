492fa58a135f663709c9d578104d889ba5d447fa
Kyle <kylehorton33@gmail.com>
2020-11-06 19:39:52 -0500

    move circle with mouse

--- learn-love/ch16/main.lua
+++ learn-love/ch16/main.lua
@@ -14,18 +14,21 @@ function love.update(dt)
   mouse_x, mouse_y = love.mouse.getPosition()
 
   angle = math.atan2(mouse_y - circle.y, mouse_x - circle.x)
+
+  cos = math.cos(angle)
+  sin = math.sin(angle)
+
+  --Make the circle move towards the mouse
+  circle.x = circle.x + circle.speed * cos * dt
+  circle.y = circle.y + circle.speed * sin * dt
 end
 
 function love.draw()
   love.graphics.circle("line", circle.x, circle.y, circle.radius)
 
-  --Print the angle
-  love.graphics.print("angle: " .. angle, 10, 10)
-
-  --Here are some lines to visualize the velocities
+  --The angle
+  love.graphics.line(circle.x, circle.y, mouse_x, mouse_y)
   love.graphics.line(circle.x, circle.y, mouse_x, circle.y)
   love.graphics.line(circle.x, circle.y, circle.x, mouse_y)
 
-  --The angle
-  love.graphics.line(circle.x, circle.y, mouse_x, mouse_y)
 end
