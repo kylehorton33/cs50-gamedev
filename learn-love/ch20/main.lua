4421117edd3dbe1e865dd3709c8ecdefc7cf6d67
Kyle <kylehorton33@gmail.com>
2020-11-06 21:32:53 -0500

    print statements to assess bugs, shows nil

--- learn-love/ch20/main.lua
+++ learn-love/ch20/main.lua
@@ -1,3 +1,5 @@
+io.stdout:setvbuf("no")
+
 function love.load()
   circle = {x = 100, y = 100}
   bullets = {}
@@ -6,7 +8,15 @@ end
 function love.update(dt)
   for i,v in ipairs(bullets) do
       v.x = v.x + 400 * dt
+      print(v.x)
   end
+end
+
+function shoot()
+  table.insert(bullets, {circle.x, circle.y})
+
+  -- Did you know that print takes infinite amount of arguments?
+  print("How many bullets?", #bullets)
 end
 
 function love.draw()
@@ -18,6 +28,9 @@ function love.draw()
 end
 
 function love.keypressed()
+  -- Adding texts like these gives context to your print.
+  -- This is especially useful when you have multiple prints.
+  print("What is the key?", key)
   if key == "space" then
       shoot()
   end
