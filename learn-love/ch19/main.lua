0173cccc3b00da78f65a440bcbfdc152c70dd4df
Kyle <kylehorton33@gmail.com>
2020-11-06 21:26:26 -0500

    add sfx and adjust volume

--- learn-love/ch19/main.lua
+++ learn-love/ch19/main.lua
@@ -2,4 +2,16 @@ function love.load()
   song = love.audio.newSource("song.ogg", "stream")
   song:setLooping(true)
   song:play()
+
+  -- sfx is short for 'sound effect', or at least I use it like that.
+  sfx = love.audio.newSource("sfx.ogg", "static")
+
+  song:setVolume(0.2)
+  sfx:setVolume(0.9)
+end
+
+function love.keypressed(key)
+  if key == "space" then
+      sfx:play()
+  end
 end
