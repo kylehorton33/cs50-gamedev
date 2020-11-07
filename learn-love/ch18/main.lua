a842766722ebba3ec2d6655dc2fa292aa578e215
Kyle <kylehorton33@gmail.com>
2020-11-06 20:37:08 -0500

    use ipairs

--- learn-love/ch18/main.lua
+++ learn-love/ch18/main.lua
@@ -9,17 +9,9 @@ function love.load()
 end
 
 function love.draw()
-  --Let's do it without ipairs first.
-
-  --For i=1 till the number of values in tilemap
-  for i=1,#tilemap do
-      --For j till the number of values in this row
-      for j=1,#tilemap[i] do
-          --If the value on row i, column j equals 1
-          if tilemap[i][j] == 1 then
-              --Draw the rectangle.
-              --Use i and j to position the rectangle.
-              -- j for x, i for y.
+  for i,row in ipairs(tilemap) do
+      for j,tile in ipairs(row) do
+          if tile == 1 then
               love.graphics.rectangle("line", j * 25, i * 25, 25, 25)
           end 
       end
