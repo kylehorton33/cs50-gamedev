408d2d8a1dda780ec82aabe051a3c188f7a2978f
Kyle <kylehorton33@gmail.com>
2020-11-06 23:41:39 -0500

    collision detection

--- learn-love/ch21/main.lua
+++ learn-love/ch21/main.lua
@@ -21,6 +21,15 @@ function love.load()
   end
 end
 
+function checkCollision(p1, p2) 
+  -- Calculating distance in 1 line
+  -- Subtract the x's and y's, square the difference
+  -- Sum the squares and find the root of the sum.
+  local distance = math.sqrt((p1.x - p2.x)^2 + (p1.y - p2.y)^2)
+  -- Return whether the distance is lower than the sum of the sizes.
+  return distance < p1.size + p2.size
+end
+
 function love.update(dt)
   if love.keyboard.isDown("left") then
       player.x = player.x - 200 * dt
@@ -32,6 +41,13 @@ function love.update(dt)
       player.y = player.y - 200 * dt
   elseif love.keyboard.isDown("down") then
       player.y = player.y + 200 * dt
+  end
+
+  for i,v in ipairs(coins) do
+      if checkCollision(player, v) then
+          table.remove(coins, i)
+          player.size = player.size + 1
+      end
   end
 end
 
