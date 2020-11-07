b739fc130328eea9d16e21900014b72d8c3e7d7f
Kyle <kylehorton33@gmail.com>
2020-11-06 21:51:07 -0500

    buggy code

---
+++ learn-love/ch20b/main.lua
@@ -0,0 +1,23 @@
+Object = require "classic"
+
+Circle = Object:extend()
+
+function Circle:new()
+    self.x = 100
+    self.y = 100
+end
+
+function Circle:draw(offset)
+    love.graphics.circle("fill", self.x + offset, self.y, 25)
+end
+
+function love.load()
+    circle = Circle()
+end
+
+function love.draw()
+    circle:draw(10)
+    circle:draw(70)
+    circle.draw(140)
+    circle:draw(210)
+end
