cf7882975901d7c2525efd2a8f4d9afa26f27de4
Kyle <kylehorton33@gmail.com>
2020-11-08 16:43:50 -0500

    fix collision errors

--- learn-love/ch23/entity.lua
+++ learn-love/ch23/entity.lua
@@ -24,11 +24,27 @@ end
 
 function Entity:resolveCollision(e)
   if self:checkCollision(e) then
-      -- The player's right side is x + width
-      -- The wall's left side is x
-      -- Calculate the difference and subtract that from the player's position
-      local pushback = self.x + self.width - e.x
-      self.x = self.x - pushback
+      if self:wasVerticallyAligned(e) then
+          if self.x + self.width/2 < e.x + e.width/2  then
+              -- pusback = the right side of the player - the left side of the wall
+              local pushback = self.x + self.width - e.x
+              self.x = self.x - pushback
+          else
+              -- pusback = the right side of the wall - the left side of the player
+              local pushback = e.x + e.width - self.x
+              self.x = self.x + pushback
+          end
+      elseif self:wasHorizontallyAligned(e) then
+          if self.y + self.height/2 < e.y + e.height/2 then
+              -- pusback = the bottom side of the player - the top side of the wall
+              local pushback = self.y + self.height - e.y
+              self.y = self.y - pushback
+          else
+              -- pusback = the bottom side of the wall - the top side of the player
+              local pushback = e.y + e.height - self.y
+              self.y = self.y + pushback
+          end
+      end
   end
 end
 
@@ -40,4 +56,16 @@ function Entity:checkCollision(e)
   and self.x < e.x + e.width
   and self.y + self.height > e.y
   and self.y < e.y + e.height
+end
+
+function Entity:wasVerticallyAligned(e)
+  -- It's basically the collisionCheck function, but with the x and width part removed.
+  -- It uses last.y because we want to know this from the previous position
+  return self.last.y < e.last.y + e.height and self.last.y + self.height > e.last.y
+end
+
+function Entity:wasHorizontallyAligned(e)
+  -- It's basically the collisionCheck function, but with the y and height part removed.
+  -- It uses last.x because we want to know this from the previous position
+  return self.last.x < e.last.x + e.width and self.last.x + self.width > e.last.x
 end
