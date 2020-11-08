4d938d767605ebbf9a4e99db9d6a0bc19e49e43f
Kyle <kylehorton33@gmail.com>
2020-11-08 16:49:26 -0500

    create pushable box

--- learn-love/ch23/main.lua
+++ learn-love/ch23/main.lua
@@ -1,22 +1,40 @@
 function love.load()
-  -- First require classic, since we use it to create our classes.
   Object = require "classic"
-  -- Second require Entity, since it's the baseclass for our other classes.
   require "entity"
   require "player"
   require "wall"
+  -- Require box!
+  require "box"
 
   player = Player(100, 100)
   wall = Wall(200, 100)
+  box = Box(400, 150)
+
+  objects = {}
+  table.insert(objects, player)
+  table.insert(objects, wall)
+  table.insert(objects, box)
 end
 
 function love.update(dt)
-  player:update(dt)
-  wall:update(dt)
-  player:resolveCollision(wall)
+  -- Update all the objects
+  for i,v in ipairs(objects) do
+      v:update(dt)
+  end
+
+  -- Go through all the objects (except the last)
+  for i=1,#objects-1 do
+      -- Go through all the objects starting from the position i + 1
+      for j=i+1,#objects do
+          objects[i]:resolveCollision(objects[j])
+      end
+  end
 end
 
+
 function love.draw()
-  player:draw()
-  wall:draw()
+  -- Draw all the objects
+  for i,v in ipairs(objects) do
+      v:draw()
+  end
 end
